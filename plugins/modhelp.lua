012029928c6a26656885ccd926939a9bc7e5b17a
sajjad-021 <sajjad-021@users.noreply.github.com>
2017-01-26 02:57:29 +0330

    Update modhelp.lua

--- plugins/modhelp.lua
+++ plugins/modhelp.lua
@@ -128,7 +128,7 @@ if msg.to.type == 'channel' and is_momod(msg) then
 ➰شما میتوانید از '/' یا '!' یا '#' برای اجرای دستورات استفاده کنید.
 ➖➖➖➖➖➖➖
 ➰ ʝօìղ մʂ ìժ çհ :
-➰ @antispamandhack
+➰ @MemberPlus_TM
 ➖➖➖➖➖➖➖ ]]
 end
 end
